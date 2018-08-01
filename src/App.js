405e1e17acec46b6c7338b2f87bd8c43980ff403
mariarojo <mariatrojo@gmail.com>
2018-07-31 19:55:18 -0700

    react practice demo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 
 
-const List = (props) => {
+const Language = (props) => {
 
    return (
-	   <h3> To Do List: </h3>
+		<div>
+		   <h3>{props.language}</h3>
+		   <button onClick = { () => {props.onChange(props.index) } }>Change</button>
+		</div>
    )
 }
 
@@ -14,11 +17,41 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			languages: ['React', 'Redux', 'Node', 'Express']
 		}
+		this.change = this.change.bind(this);
+	}
+
+	change(index){
+		this.state.languages[index] = 'changed';
+		this.setState({
+			languages: this.state.languages
+		})
 	}
 
 	render() {
-		return <List />
+		return (
+			<div>
+				{this.state.languages.map( (language, index) => {
+					return (
+						
+						// <div key={index}>
+						// 	{language}
+						// 	<button onClick={()=>{ this.change(index) }}>Change</button>
+						// </div>
+
+						<Language 
+							key={index} 
+							language={language} 
+							onChange={this.change}
+							index={index}
+							// onChange={ ()=>{this.change(index)} }
+						/>
+					)
+				} ) }
+
+			</div>
+		)
 	}
 
 }
