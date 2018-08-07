b348176993a7174289b0f4245c5b68735e3c8776
mariarojo <mariatrojo@gmail.com>
2018-08-07 15:14:23 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,58 +1,78 @@
 import React from 'react';
 
-
-
-const Language = (props) => {
-
-   return (
+const TodoCreate = (props) => {
+	return (
 		<div>
-		   <h3>{props.language}</h3>
-		   <button onClick = { () => {props.onChange(props.index) } }>Change</button>
+			<form>
+				<input type="text" />
+				<input type="submit" />
+			</form>
 		</div>
-   )
+	)
+	
 }
 
-class App extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			languages: ['React', 'Redux', 'Node', 'Express']
-		}
-		this.change = this.change.bind(this);
-	}
-
-	change(index){
-		this.state.languages[index] = 'changed';
-		this.setState({
-			languages: this.state.languages
-		})
-	}
-
-	render() {
+const TodoList = (props) => {
+	const list = props.items.map((item, index) => {
 		return (
-			<div>
-				{this.state.languages.map( (language, index) => {
-					return (
-						
-						// <div key={index}>
-						// 	{language}
-						// 	<button onClick={()=>{ this.change(index) }}>Change</button>
-						// </div>
-
-						<Language 
-							key={index} 
-							language={language} 
-							onChange={this.change}
-							index={index}
-						/>
-					)
-				} ) }
-
-			</div>
+			<li key={index}>
+				{item.id} - {item.text} - {item.completed.toString()}
+			</li>
 		)
+	});
+	return (
+		<ul>
+			{list}
+		</ul>
+	)
+ }
+ 
+ class App extends React.Component {
+ 
+	 constructor(props) {
+		 super(props);
+		 this.state = {
+			 items: [
+				 { id: 1, text: "Learn React", completed: true},
+				 { id: 2, text: "Build a todo app", completed: false},
+				 { id: 3, text: "Profit", completed: false}
+			 ],
+			 filter: 'all'
+		 }
+		 this.addTodo = this.addTodo.bind(this);
+		 this.updateTodo = this.updateTodo.bind(this);
+		 this.removeTodo = this.removeTodo.bind(this);
+	 }
+ 
+	 addTodo(index){
+		 this.state.languages[index] = 'changed';
+		 this.setState({
+			 languages: this.state.languages
+		 })
+	 }
+
+	 updateTodo(index){
 	}
 
-}
+	removeTodo(index){
+
+	}
+ 
+	 render() {
+		 return (
+			 <div>
+				 <TodoCreate
+				//  addTodo={this.addTodo}
+				 />
+				 <TodoList
+				 items = {this.state.items}
+				 updateTodo = {this.updateTodo}
+				 removeTodo = {this.removeTodo}
+				 />
+			 </div>
+		 )
+	 }
+ 
+ }
 
 export default App;
